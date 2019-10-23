80a97d60b1775c003c8e3fd3f14cbef88ac2eb35
mariojohnson88 <johnson.mario8404@gmail.com>
2019-10-23 09:21:08 -0500

    3 files modified, 2 added

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,18 +1,28 @@
 export const products = [
   {
-    name: 'Phone XL',
-    price: 799,
+    name: 'Samsung Galaxy Note S10',
+    price: 950,
     description: 'A large phone with one of the best screens'
   },
   {
-    name: 'Phone Mini',
+    name: 'iPhone 11',
     price: 699,
     description: 'A great phone with one of the best cameras'
   },
   {
-    name: 'Phone Standard',
-    price: 299,
+    name: 'Google Pixel 4',
+    price: 899,
     description: ''
+  },
+  {
+    name: 'OnePlus 7T Pro',
+    price: 599,
+    description: 'A fantastic phone in most departments'
+  },
+  {
+    name: 'Nokia 8.2 5G',
+    price: 500,
+    description: 'The Nokia 8.2 could one of the more affordable 5G smartphones'
   }
 ];
 
